56d2e69c3957f21114852c9795d5d5ee4461b792
TUSHAR292004 <tusharchandak2004@gmail.com>
2024-04-18 20:45:27 +0530

    landing-page

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,25 +5,28 @@ import { leftSideVariants, rightSideVariants } from "../constants/motion"
 
 const About = () => {
     return (
-        <section id="about" className="bg-black">
+        <section id="about" className="bg-black pt-0">
             <div className="container">
-                <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-8">
+                <div className="flex flex-col items-center justify-between gap-8">
                     {/* left side */}
                     <motion.div
                         variants={leftSideVariants}
                         initial="hidden"
                         whileInView="visible"
                         viewport={{ once: true }}
-                        className="flex flex-col gap-y-8 lg:w-1/2 w-auto flex-shrink-0 text-center lg:text-star">
-                        <div className="flex flex-col gap-y-4 text-start">
-                            <p className="text-gray-80">WHY SUPERNOVA?</p>
-                            <h2 className="relative text-[3.5rem] font-bold leading-[3.25rem] w-full text-white">
-                            Quality schools out of reach for 250M Indian Children.
-                            </h2>
-                            <p className="text-bold font-lg text-gray-80">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium deserunt incidunt quia non vitae voluptates, adipisci sit dolore quisquam eos repudiandae laborum culpa minima beatae velit ut! Magni, asperiores deserunt?
-                            </p>
+                        className="flex flex-col md:flex-row justify-between gap-y-8 w-[90%] lg:w-[100%] flex-shrink-0 text-center md:text-start">
+                        <div className="flex flex-row gap-y-4 ">
+                            <div className="md:w-[40vw]">
+                                <p className="text-gray-80 mb-[2vh]" >WHY SUPERNOVA?</p>
+                                <h2 className="relative text-[2rem] leading-8 md:leading-[60px] md:text-[4rem] font-bold  w-full text-white">
+                                    Quality schools out of reach for 250M Indian Children.
+                                </h2>
+                            </div>
+                            
                         </div>
+                        <p className="text-bold text-[1.2rem] md:text-[1.4rem] md:width-[30vw] text-[#ffffff75]">
+                               Lorem ipsum dolor sit amet consectetur adipisicing elit. In eos consectetur debitis, beatae illum dolorum expedita? Velit suscipit iste, expedita distinctio, consequatur nihil accusamus exercitationem asperiores tempore eum corrupti impedit.
+                            </p>
                     </motion.div>
                     {/* right side */}
                     <motion.div
@@ -31,9 +34,18 @@ const About = () => {
                         initial="hidden"
                         whileInView="visible"
                         viewport={{ once: true }}
-                        className="flex lg:w-1/2 w-full">
-                        <div className="">
-
+                        className="flex flex-col-reverse md:flex-row justify-between gap-y-8 w-[90%] lg:w-[100%] flex-shrink-0 text-center md:text-start">
+                            <p className="text-bold text-[1.2rem] md:text-[1.4rem] md:width-[30vw] text-[#ffffff75] ">
+                               Lorem ipsum dolor sit amet consectetur adipisicing elit. In eos consectetur debitis, beatae illum dolorum expedita? Velit iste, expedita distinctio, consequatur nihil accusamus exercitationem asperiores tempore eum corrupti impedit.
+                            </p>
+                        <div className="md:ml-[1vw] flex flex-row gap-y-4 md:pl-[4vw]">
+                            <div className="md:w-[40vw]">
+                                <p className="text-gray-80 mb-[2vh]" >WHY SUPERNOVA?</p>
+                                <h2 className="relative text-[2rem] leading-8 md:leading-[60px] md:text-[4rem] font-bold  w-full text-white ">
+                                    Quality schools out of reach for.
+                                </h2>
+                            </div>
+                            
                         </div>
                     </motion.div>
                 </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,43 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 import { leftSideVariants, rightSideVariants } from "../constants/motion"
 import { hero01, hero02, hero03, roundedText, hero1, playStore, appleStore } from "../constants/images";
 const Hero = () => {
+    useEffect(() => {
+        const handleScroll = () => {
+            const line1 = document.querySelector('.line');
+            if (line1) {
+                const pos = window.scrollY - 30;
+                
+                line1.style.right = `${pos*2}px`;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const line1 = document.querySelector('.line-1');
+            if (line1) {
+                const pos = window.scrollY - 300;
+                
+                line1.style.left = `${pos*0.5}px`;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
         <section id="home" className="flex  bg-black pt-[10px]">
             <div className="container flex flex-col items-center md:mt-[6vh]">
@@ -27,17 +63,16 @@ const Hero = () => {
                         initial='hidden'
                         animate="visible"
                         className="flex justify-center lg:w-1/2">
-                        <div className="w-[60%]">
-
+                        <div className="w-[70%] md:ml-[8vw]">
                             <img src={hero1} alt="" className="rounded-4xl" />
                         </div>
-                        {/* <div className="hidden sm:block w-[120px] h-[120px] hover:rotate-45 absolute top-1/2  -translate-x-1/2 -translate-y-1/2 left-[calc(50%-15px)] transition ease-in-out">
-                            <img src={roundedText} alt="Explore More" />
-                        </div> */}
                     </motion.div>
                 </div>
                 <div className="mt-[15vh]">
-                    <span className="text-[5rem]  text-white">
+                    <span className="line absolute top-[60%] text-[6rem] font-extrabold right-[50px] text-[#6600CA] whitespace-nowrap md:block hidden">
+                        The future is here!
+                    </span>
+                    <span className="line-1 absolute top-[80%] left-[300px] font-extrabold text-[5rem]  text-[#6600CA] whitespace-nowrap md:hidden">
                         The future is here!
                     </span>
                 </div>
