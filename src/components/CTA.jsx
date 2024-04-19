77e650cfae68c306723a36c6ec9a4a248ca121f8
TUSHAR292004 <tusharchandak2004@gmail.com>
2024-04-19 23:51:53 +0530

    committed

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -11,14 +11,14 @@ const CTA = () => {
             initial="hidden"
             whileInView="visible"
             viewport={{margin:'0px 0px -200px 0px', once:true}}
-            className="container h-[400px]">
-                <div className="flex flex-row justify-evenly items-center gap-y-8 w-full h-full rounded-xl bg-[#6823EB] pt-[4vh]">
-                    <div className="text-white w-[30%]">
-                        <h1 className="text-[2rem] font-bold my-2">Get in Touch!</h1>
-                        <p className="text-[1.5rem] my-2">Interested in joining our AI Spoken English course? </p>
-                        <button className="text-[1.2rem] flex items-center font-semibold bg-[#FF0062] rounded-lg p-[20px] my-[6vh] hover:bg-[#4400C3] transition-all duration-300 ease-in-out">Whatsapp Us <FaAngleRight className="ml-4 text-[1.2rem]"/></button>
+            className="container md:h-[400px] flex justify-center">
+                <div className="flex flex-col md:flex-row justify-evenly items-center gap-y-8 w-[80%] md:w-full h-full rounded-xl bg-[#6823EB] pt-[4vh]">
+                    <div className="text-white w-[80%] md:w-[30%] md:text-start text-center">
+                        <h1 className=" text-[2rem] font-bold my-2">Get in Touch!</h1>
+                        <p className=" text-[1.5rem] my-2">Interested in joining our AI Spoken English course? </p>
+                        <p className="text-[1.2rem] flex justify-center items-center font-semibold bg-[#FF0062] rounded-lg p-[20px] my-[6vh] hover:bg-[#4400C3] transition-all duration-300 ease-in-out">Whatsapp Us <FaAngleRight className="ml-4 text-[1.2rem]"/></p>
                     </div>
-                    <div className="w-[40%] h-[100%]">
+                    <div className="w-[80%] md:w-[40%] h-[100%] flex justify-center">
                         <img src={contactUs} alt="" className="w-[80%]" />
                     </div>
                 </div>
