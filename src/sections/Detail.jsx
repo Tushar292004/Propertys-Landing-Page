2cefa53ad06c3fa4a318f2e774cfb322b237d79b
TUSHAR292004 <tusharchandak2004@gmail.com>
2024-04-19 13:10:14 +0530

    commits

--- src/sections/Detail.jsx
+++ src/sections/Detail.jsx
@@ -20,21 +20,31 @@ function Detail() {
         <section id="detail" className="bg-black">
             <div className="container flex flex-col md:flex-row  md:w-[100%] w-[80%]">
                 {/* left side */}
-                <div className="text-white text-[2.5rem] font-bold md:w-[30%] md:text-start text-center">
+                <motion.div
+                variants={leftSideVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true}}
+                className="text-white text-[2rem] mb-4 d:text-[2.5rem] font-bold md:w-[30%] md:text-start text-center">
                     Frequently Asked Questions 🤔
-                </div>
+                </motion.div>
                 {/* right side */}
-                <div className="flex md:ml-[2vh] md:w-[70%] flex-col">
+                <motion.div 
+                variants={rightSideVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true}}
+                className="flex md:ml-[2vh] md:w-[70%] flex-col">
                     {faqContent.map((content) => (
                         <div className='border-b-2 border-b-gray-20 py-[4vh]'>
                             <div className="flex w-full justify-between cursor-pointer " onClick={() => toggleContent(content.title)}>
-                                <h1 key={content.title} className='text-white text-[2rem]'>{content.title}</h1>
-                                <FaAngleDown className={`text-white text-[2rem] transform transition-transform duration-300 ease-in-out ${expandedState[content.title] ? 'rotate-180' : ''}`} />
+                                <h1 key={content.title} className='text-white text-[1.5rem] md:text-[2rem]'>{content.title}</h1>
+                                <FaAngleDown className={`text-white text-[1.5rem] md:text-[2rem] transform transition-transform duration-300 ease-in-out ${expandedState[content.title] ? 'rotate-180' : ''}`} />
                             </div>
-                            {expandedState[content.title] && <div className='text-[#ffffff80] text-[1.5rem] transition-all duration-300 ease-in-out'>{content.description}</div>}
+                            {expandedState[content.title] && <div className='text-[#ffffff80] text-[1.3rem] md:text-[1.5rem] transition-all duration-300 ease-in-out'>{content.description}</div>}
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     )
